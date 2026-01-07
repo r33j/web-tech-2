eb9751454c447890a8018b85cc325c334f0e9b27
Mohammad Alug <malug37831@liceo.edu.ph>
2026-01-07 12:57:41 +0800

    Prelim Exam Submission

---
+++ src/app/prelim-exam/prelim-exam.component.ts
@@ -0,0 +1,11 @@
+import { Component, signal } from "@angular/core";
+
+@Component({
+    selector: 'app-prelim-exam',
+    templateUrl: './prelim-exam.component.html',
+    styleUrls: ['./prelim-exam.component.scss']
+})
+export class PrelimExamComponent {
+
+public title = signal ('Prelim Exam');
+}
